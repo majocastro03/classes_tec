b4d9031bd12fa201ac79627692f1375ba690bf83
Maria Jose <maria.romero@softlogyco.com>
2025-09-29 08:49:30 -0500

    Implement task management methods to add and remove tasks in Tareas component

--- monitorias/Módulo 2 - Herramientas y Lenguajes de Programación/app-task-list/src/app/tareas/tareas.ts
+++ monitorias/Módulo 2 - Herramientas y Lenguajes de Programación/app-task-list/src/app/tareas/tareas.ts
@@ -9,5 +9,16 @@ import { Tarea } from '../models/tarea';
   styleUrl: './tareas.css'
 })
 export class Tareas {
-  
+  tareas: Tarea[] = [];
+  nuevaTarea: Tarea = { id: 0, descripcion: '', completada: false };
+  agregarTarea() {
+    if (this.nuevaTarea.descripcion.trim() !== '') {
+      this.tareas.push(this.nuevaTarea);
+      this.nuevaTarea = { id: 0, descripcion: '', completada: false };
+    }
+  }
+
+  eliminarTarea(index: number) {
+    this.tareas.splice(index, 1);
+  }
 }
